e9ac053cd5a1b8370ef468063722d1905bc6b566
yuanxiaotian <781414750@qq.com>
2018-05-28 16:26:46 +0800

    2018/5/18 18:47 V1.0.3更新

--- Lib_Request/src/main/java/com/cangmaomao/network/request/config/Config.java
+++ Lib_Request/src/main/java/com/cangmaomao/network/request/config/Config.java
@@ -6,7 +6,7 @@ public class Config {
 //    public static final String S_HTTP_ROOT_URL = "http://www.seteat.com/";
 
     //全局url2  https://codeload.github.com/yuanxiaotian/Request/zip/master
-    public static final String S_HTTP_ROOT_URL = "https://codeload.github.com/";
+    public static String S_HTTP_ROOT_URL = "https://codeload.github.com/";
 
     //全局3  https://g37.gdl.netease.com/onmyoji_netease_23_1.0.40.apk
 //    public static final String S_HTTP_ROOT_URL = "https://g37.gdl.netease.com/";
