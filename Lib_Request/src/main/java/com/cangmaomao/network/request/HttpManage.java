965e6305e07a15ff723f75a2e7eeca86662a9dec
CangMaoMao <781414750@qq.com>
2018-06-21 15:40:39 +0800

    2018/06.21 15：40

--- Lib_Request/src/main/java/com/cangmaomao/network/request/HttpManage.java
+++ Lib_Request/src/main/java/com/cangmaomao/network/request/HttpManage.java
@@ -11,10 +11,12 @@ import com.cangmaomao.network.request.utils.RxSchedulers;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -24,6 +26,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HttpManage {
 
     private Retrofit retrofit;
+    private OkHttpClient client;
     private DownloadInterceptor downloadInterceptor;
 
     private static class HttpManageHolder {
@@ -38,11 +41,9 @@ public class HttpManage {
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         downloadInterceptor = new DownloadInterceptor();
-        OkHttpClient client = new OkHttpClient.Builder()
+        client = new OkHttpClient.Builder()
                 .addNetworkInterceptor(interceptor)
                 .addInterceptor(downloadInterceptor)
-                .addInterceptor(AbsInterceptor.readInterceptor)
-                .addInterceptor(AbsInterceptor.writeInterceptor)
                 .retryOnConnectionFailure(true)
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
@@ -59,6 +60,10 @@ public class HttpManage {
 
     public <T> T create(Class<T> clazz) {
         return retrofit.create(clazz);
+    }
+
+    public List<Interceptor> getInterceptors() {
+        return client.interceptors();
     }
 
     /**
