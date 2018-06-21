39abe1439f34a599eea88ba02efb02dc4ecad8a9
CangMaoMao <781414750@qq.com>
2018-06-21 17:33:09 +0800

    2018/06.21 15：40

--- Lib_Request/src/main/java/com/cangmaomao/network/request/cookie/AbsCookieJar.java
+++ Lib_Request/src/main/java/com/cangmaomao/network/request/cookie/AbsCookieJar.java
@@ -1,13 +1,10 @@
 package com.cangmaomao.network.request.cookie;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 
 
 public class AbsCookieJar {
 
-
-    @SuppressLint("StaticFieldLeak")
-    public static Context mContext = null;
+    public static Context mContext;
 
 }
