7e26bcc5332240f0e38083b6f4be40dd20491d47
CangMaoMao <781414750@qq.com>
2018-06-29 16:08:50 +0800

    2018-06-29

--- Lib_Request/src/main/java/com/cangmaomao/network/request/utils/RxSchedulers.java
+++ Lib_Request/src/main/java/com/cangmaomao/network/request/utils/RxSchedulers.java
@@ -1,37 +1,14 @@
 package com.cangmaomao.network.request.utils;
 
-import com.cangmaomao.network.request.RxHttpMange;
-
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscription;
-
-import io.reactivex.Flowable;
-import io.reactivex.FlowableTransformer;
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class RxSchedulers {
-
-
     public static <T> ObservableTransformer<T, T> io_main() {
-        return new ObservableTransformer<T, T>() {
-            @Override
-            public ObservableSource<T> apply(Observable<T> upstream) {
-                return upstream.subscribeOn(Schedulers.io())
-                        .doOnSubscribe(new Consumer<Disposable>() {
-                            @Override
-                            public void accept(Disposable disposable) throws Exception {
-                                RxHttpMange.getInstance().removeAll();
-                            }
-                        })
+        return upstream ->
+                upstream.subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread());
-            }
 
-        };
     }
 }
