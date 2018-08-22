a3a2794b956ad5e4ded9a465fd6bb69345f55435
藏猫猫 <781414750@qq.com>
2018-08-22 11:58:50 +0800

    增加一个无进度上传功能,修复上传线程管理问题

--- app/src/main/java/com/cangmaomao/network/myapplication/MainActivity.kt
+++ app/src/main/java/com/cangmaomao/network/myapplication/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.cangmaomao.network.myapplication
 
+<<<<<<< Updated upstream
 import android.content.Intent
+=======
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.support.v7.app.AppCompatActivity
+>>>>>>> Stashed changes
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.cangmaomao.network.request.HttpManage
@@ -8,6 +14,8 @@ import com.cangmaomao.network.request.RxHttpMange
 import com.cangmaomao.network.request.utils.RxSchedulers
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,12 +33,57 @@ class MainActivity : AppCompatActivity() {
         map["ct"] = 1
         map["password"] = 1212
         map["loginid"] = 1212
-
         RxHttpMange.getInstance().add(MainActivity::class.java.simpleName, loading())
 
+<<<<<<< Updated upstream
+        RxHttpMange.getInstance().add(MainActivity::class.java.simpleName, loading())
+=======
+        createFiles("test")
+>>>>>>> Stashed changes
 
+        val options = BitmapFactory.Options()
+
+<<<<<<< Updated upstream
         appCompatButton.setOnClickListener { startActivity(Intent(this,IndexActivity::class.java)) }
+=======
+        options.inSampleSize = 2
+
+        options.inJustDecodeBounds = false
+
+
+        val bitmap = BitmapFactory.decodeFile(externalCacheDir.absolutePath + "/test/timg.jpg", options)
+
+
+
+        appCompatImageView.setImageBitmap(bitmap)
+
+
+        saveDraw(externalCacheDir.absolutePath + "/test","test1",bitmap)
+>>>>>>> Stashed changes
     }
+
+    //生成文件文件夹
+    fun createFiles(loginId: String) {
+        val accountFile = externalCacheDir.absolutePath + "/" + loginId
+        val file = File(accountFile)
+        if (!file.exists())
+            file.mkdirs()
+    }
+
+
+    //保存画板内容
+    fun saveDraw(path: String, name: String, bitmap: Bitmap?) {
+        val fileName = "$name.jpg"
+        val file = File(path, fileName)
+        val fos = FileOutputStream(file)
+        if (bitmap == null) {
+            return
+        }
+        bitmap.compress(Bitmap.CompressFormat.WEBP, 80, fos)
+        fos.flush()
+        fos.close()
+    }
+
 
     private fun loading(): Disposable {
         return HttpManage.getInstance()
